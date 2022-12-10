9632696a998f4e184b2ded7218697e9f0d302e8b
sarahdavies186 <sarahdavies186@gmail.com>
2022-12-10 17:29:41 +0000

    Uploaded

---
+++ script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const name = document.getElementById("name");
+const emailError = document.getElementById("error");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "You need to enter an e-mail address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered value needs to be an e-mail address.";
+  }
+}
